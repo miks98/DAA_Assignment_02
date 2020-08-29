2fb8801d14c3550a7cfb6dbf8e4559eafda6e647
M Iftikhar Uddin Khan Sami <hits89411a@gmail.com>
2020-08-29 17:10:32 +0500

    ASS_02_Q_01

---
+++ assignment02/src/Q1/QuickSort/case1_1.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Q1.QuickSort;
+
+import java.util.Random;
+
+/**
+ *
+ * @author MIKS
+ */
+public class case1_1 {
+       /* Java program for Merge Sort */
+	// Java program for implementation of QuickSort 
+
+	int partition(int arr[], int low, int high) 
+	{ 
+		int pivot = arr[high]; 
+		int i = (low-1); // index of smaller element 
+		for (int j=low; j<high; j++) 
+		{ 
+			// If current element is smaller than the pivot 
+			if (arr[j] < pivot) 
+			{ 
+				i++; 
+
+				// swap arr[i] and arr[j] 
+				int temp = arr[i]; 
+				arr[i] = arr[j]; 
+				arr[j] = temp; 
+			} 
+		} 
+
+		// swap arr[i+1] and arr[high] (or pivot) 
+		int temp = arr[i+1]; 
+		arr[i+1] = arr[high]; 
+		arr[high] = temp; 
+
+		return i+1; 
+	} 
+
+
+	/* The main function that implements QuickSort() 
+	arr[] --> Array to be sorted, 
+	low --> Starting index, 
+	high --> Ending index */
+	void sort(int arr[], int low, int high) 
+	{ 
+		if (low < high) 
+		{ 
+			/* pi is partitioning index, arr[pi] is 
+			now at right place */
+			int pi = partition(arr, low, high); 
+
+			// Recursively sort elements before 
+			// partition and after partition 
+			sort(arr, low, pi-1); 
+			sort(arr, pi+1, high); 
+		} 
+	} 
+
+	/* A utility function to print array of size n */
+	static void printArray(int arr[]) 
+	{ 
+		int n = arr.length; 
+		for (int i=0; i<n; ++i) 
+			System.out.print(arr[i]+" "); 
+		System.out.println(); 
+	} 
+/*This code is contributed by Rajat Mishra */
+
+	// Driver method 
+	public static void main(String args[]) 
+	{ 
+		int arr[]= new int[100]; 
+                Random rInt = new Random();
+                for (int i = 0; i < arr.length; i++) {
+                    arr[i] = i;
+            }
+
+		System.out.println("Given Array"); 
+		printArray(arr); 
+                
+                System.out.println("Length of Given Array = "+ arr.length); 
+                
+
+		case1_1 ob = new case1_1(); 
+		ob.sort(arr, 0, arr.length - 1); 
+
+		System.out.println("\nSorted array"); 
+		printArray(arr); 
+	} 
+}
